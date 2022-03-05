26b343a8556c3bbfaf3c82a9227def60623bb5a6
Yuchiaoniu <97222721+Yuchiaoniu@users.noreply.github.com>
2022-03-05 21:46:45 +0800

    Add files via upload

--- 大專整合_V3/wandering.sql
+++ 大專整合_V3/wandering.sql
@@ -2,10 +2,10 @@
 -- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
--- 主機： localhost:3306
--- 產生時間： 2022-03-05 11:33:51
--- 伺服器版本： 5.7.24
--- PHP 版本： 7.4.16
+-- Host: localhost:3306
+-- Generation Time: Mar 05, 2022 at 01:45 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,122 +18,118 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- 資料庫： `wandering`
+-- Database: `wandering`
 --
 
 -- --------------------------------------------------------
 
 --
--- 資料表結構 `adopt`
+-- Table structure for table `adopt`
 --
 
 CREATE TABLE `adopt` (
   `id` int(1) NOT NULL,
-  `memberID` int(11) NOT NULL,
+  `memberID` int(255) NOT NULL,
   `title` varchar(100) DEFAULT NULL,
-  `type` enum('狗','貓') NOT NULL,
-  `amount` int(100) NOT NULL,
+  `type` enum('狗','貓') CHARACTER SET utf8mb4 NOT NULL,
   `ear` enum('是','否') NOT NULL,
-  `chip` enum('是','否') NOT NULL,
   `ligation` enum('是','否') NOT NULL,
-  `name` varchar(10) DEFAULT NULL,
+  `nickname` varchar(10) DEFAULT NULL,
   `gender` enum('男','女') NOT NULL,
-  `city` varchar(1) DEFAULT NULL,
-  `zone` varchar(1) DEFAULT NULL,
-  `address` varchar(40) DEFAULT NULL,
-  `appearance` varchar(100) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `town` varchar(255) DEFAULT NULL,
   `img` varchar(100) DEFAULT NULL,
-  `contact` varchar(30) DEFAULT NULL,
-  `context` varchar(100) DEFAULT NULL
+  `reason` varchar(30) DEFAULT NULL,
+  `status` varchar(100) DEFAULT NULL,
+  `adoptcondition` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `adopt_city` varchar(255) CHARACTER SET utf8mb4 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- 傾印資料表的資料 `adopt`
+-- Dumping data for table `adopt`
 --
 
-INSERT INTO `adopt` (`id`, `memberID`, `title`, `type`, `amount`, `ear`, `chip`, `ligation`, `name`, `gender`, `city`, `zone`, `address`, `appearance`, `img`, `contact`, `context`) VALUES
-(1, 2, '台中市南區，親人親狗的牛牛，在等一個幸褔的家', '狗', 1, '是', '是', '是', '牛牛', '男', '5', '3', '227 Sec3 Taiwan Blvd', 'black and huge', 'images/16.png', 'cell phone 0123456789', 'lost at central park this afternoon 20210101'),
-(2, 2, '聰明的乖乖想要一個愛他的家12323145', '狗', 1, '是', '否', '是', '乖乖', '男', '5', '3', '306 Sec1 Taiwan Blvd', 'black and huge', 'images/20.png', 'cell phone 0123456789', 'near central park this afternoon 20210102'),
-(3, 12, '帥氣的茶凍想要一個溫暖的家', '狗', 1, '是', '是', '否', '茶凍', '男', '5', '3', '808 Sec6 Taiwan Blvd', 'black and tiny', 'images/30.png', 'cell phone 0123456789', 'near central park this afternoon 20210103');
+INSERT INTO `adopt` (`id`, `memberID`, `title`, `type`, `ear`, `ligation`, `nickname`, `gender`, `city`, `town`, `img`, `reason`, `status`, `adoptcondition`, `adopt_city`) VALUES
+(1, 0, '台中市南區，親人親狗的牛牛，在等一個幸褔的家', '狗', '是', '是', '牛牛', '男', '5', '3', 'images/16.png', 'cell phone 0123456789', 'lost at central park this afternoon 20210101', '', ''),
+(2, 0, '聰明的乖乖想要一個愛他的家', '狗', '是', '是', '乖乖', '男', '5', '3', 'images/20.png', 'cell phone 0123456789', 'near central park this afternoon 20210102', '', ''),
+(3, 0, '帥氣的茶凍想要一個溫暖的家', '狗', '是', '否', '茶凍', '男', '5', '3', 'images/30.png', 'cell phone 0123456789', 'near central park this afternoon 20210103', '', ''),
+(5, 0, 's', '貓', '否', '否', 'ds', '女', '新北市', '萬里區', './upload/20220305_192756.jpg', 'fds', 'fds', 'dfs', '4, 12, 全區');
 
 -- --------------------------------------------------------
 
 --
--- 資料表結構 `cofind`
---
-
-CREATE TABLE `cofind` (
-  `id` int(255) NOT NULL,
-  `memberID` int(11) NOT NULL,
-  `title` varchar(20) DEFAULT NULL,
-  `type` enum('狗','貓') DEFAULT NULL,
-  `ear` enum('是','否') DEFAULT NULL,
-  `chip` enum('是','否') DEFAULT NULL,
-  `ligation` enum('是','否') CHARACTER SET utf8mb4 DEFAULT NULL,
-  `name` varchar(10) DEFAULT NULL,
-  `gender` enum('男','女') DEFAULT NULL,
-  `city` varchar(1) DEFAULT NULL,
-  `zone` varchar(1) DEFAULT NULL,
-  `address` varchar(40) DEFAULT NULL,
-  `appearance` varchar(100) DEFAULT NULL,
-  `img` varchar(100) DEFAULT NULL,
-  `contact` varchar(30) DEFAULT NULL,
-  `context` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- 傾印資料表的資料 `cofind`
---
-
-INSERT INTO `cofind` (`id`, `memberID`, `title`, `type`, `ear`, `chip`, `ligation`, `name`, `gender`, `city`, `zone`, `address`, `appearance`, `img`, `contact`, `context`) VALUES
-(1, 2, '我家愛貓遺失了 請求幫助協尋!!!', '狗', '是', '是', '', 'Uni(屋逆)', '男', '5', '3', '227 Sec3 Taiwan Blvd', 'black and huge', 'images/stry1.png', 'cell phone 0123456789', 'lost at central park this afternoon 20210101'),
-(2, 12, '黑狗娜娜走失了', '狗', '是', '是', '', '娜娜', '女', '5', '3', '306 Sec1 Taiwan Blvd', 'black and huge', 'images/stry2.png', 'cell phone 0123456789', 'near central park this afternoon 20210102'),
-(3, 2, '家犬走失，感謝金一萬，拜託大家', '狗', '是', '是', '', '吉利', '男', '5', '3', '808 Sec6 Taiwan Blvd', 'black and tiny', 'images/stry3.png', 'cell phone 0123456789', 'near central park this afternoon 20210103'),
-(4, 2, 'TEST', '狗', '是', '是', '', 'amy', '男', '5', '3', '808 Sec6 Taiwan Blvd', 'TEST', 'images/head.jpg', 'cell phone 0123456789', 'near central park this afternoon 20210103'),
-(5, 11, '測試檔案', '狗', '是', '是', '是', 'amy', '男', '5', '3', '808 Sec6 Taiwan Blvd', 'TEST', 'images/cute-dog1.jpeg', 'cell phone 0123456789', 'near central park this afternoon 20210103');
-
--- --------------------------------------------------------
-
---
--- 資料表結構 `rescue`
+-- Table structure for table `rescue`
 --
 
 CREATE TABLE `rescue` (
   `id` int(1) NOT NULL,
-  `memberID` int(11) NOT NULL,
-  `title` varchar(20) DEFAULT NULL,
-  `date` date NOT NULL,
-  `type` enum('狗','貓') NOT NULL,
-  `amount` int(255) NOT NULL,
-  `city` varchar(1) DEFAULT NULL,
-  `zone` varchar(1) DEFAULT NULL,
+  `memberID` int(255) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `type` enum('狗','貓') CHARACTER SET utf8mb4 NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `amount` int(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `town` varchar(255) DEFAULT NULL,
   `address` varchar(40) DEFAULT NULL,
-  `lost` tinyint(1) DEFAULT NULL,
-  `appearance` varchar(100) DEFAULT NULL,
+  `lost` enum('是','否') CHARACTER SET utf8mb4 DEFAULT NULL,
   `img` varchar(100) DEFAULT NULL,
-  `contact` varchar(30) DEFAULT NULL,
-  `context` varchar(100) DEFAULT NULL,
-  `requirement` varchar(255) DEFAULT NULL,
-  `responsibility` varchar(255) DEFAULT NULL
+  `reason` varchar(100) DEFAULT NULL,
+  `detail` varchar(255) DEFAULT NULL,
+  `need` varchar(255) NOT NULL,
+  `responsibility` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- 傾印資料表的資料 `rescue`
+-- Dumping data for table `rescue`
 --
 
-INSERT INTO `rescue` (`id`, `memberID`, `title`, `date`, `type`, `amount`, `city`, `zone`, `address`, `lost`, `appearance`, `img`, `contact`, `context`, `requirement`, `responsibility`) VALUES
-(1, 12, '南投草屯受傷的流浪狗', '2022-02-20', '狗', 1, '5', '2', '227 Sec3 Taiwan Blvd', 0, 'black and huge', 'images/rescue1.png', 'cell phone 0123456789', 'lost at central park this afternoon 20210101', '', ''),
-(2, 12, '2/10晚上在中興大學附近撿到親人品種貓', '2022-02-28', '貓', 1, '5', '3', '306 Sec1 Taiwan Blvd', 0, 'black and huge', 'images/rescue2.png', 'cell phone 0123456789', 'near central park this afternoon 20210102', '', ''),
-(3, 2, '發現田間路旁有一隻狗，前腳好像被車撞', '2022-02-27', '狗', 1, '5', '3', '808 Sec6 Taiwan Blvd', 0, 'black and tiny', 'images/rescue3.png', 'cell phone 0123456789', 'near central park this afternoon 20210103', '', ''),
-(6, 2, '有一隻狗，前腳好像被車撞', '2022-02-27', '狗', 1, '5', '3', '808 Sec6 Taiwan Blvd', 0, 'black and tiny', 'images/16.png', 'cell phone 0123456789', 'near central park this afternoon 20210103', '', ''),
-(7, 2, '中興大學附近撿到親人品種貓', '2022-02-27', '狗', 1, '5', '3', '306 Sec1 Taiwan Blvd', 0, 'black and huge', 'images/17.png', 'cell phone 0123456789', 'near central park this afternoon 20210102', '', ''),
-(8, 2, '快點來救救我', '2022-02-20', '狗', 1, '5', '2', '227 Sec3 Taiwan Blvd', 0, 'black and huge', 'images/18.png', 'cell phone 0123456789', 'lost at central park this afternoon 20210101', '', ''),
-(9, 2, '111', '2022-03-04', '狗', 111, NULL, NULL, NULL, NULL, NULL, './upload/20220304_142836.png', NULL, NULL, NULL, NULL);
+INSERT INTO `rescue` (`id`, `memberID`, `title`, `type`, `date`, `amount`, `city`, `town`, `address`, `lost`, `img`, `reason`, `detail`, `need`, `responsibility`) VALUES
+(1, 0, '南投草屯受傷的流浪狗', '狗', '2022-02-20 07:10:38', 1, '5', '2', '227 Sec3 Taiwan Blvd', '', 'images/rescue1.png', 'lost at central park this afternoon 20210101', '', '', ''),
+(2, 0, '2/10晚上在中興大學附近撿到親人品種貓', '狗', '2022-02-27 07:10:38', 1, '5', '3', '306 Sec1 Taiwan Blvd', '', 'images/rescue2.png', 'near central park this afternoon 20210102', '', '', ''),
+(3, 0, '發現田間路旁有一隻狗，前腳好像被車撞', '狗', '2022-02-27 07:10:38', 1, '5', '3', '808 Sec6 Taiwan Blvd', '', 'images/rescue3.png', 'near central park this afternoon 20210103', '', '', '');
 
 -- --------------------------------------------------------
 
 --
--- 資料表結構 `users`
+-- Table structure for table `stray`
+--
+
+CREATE TABLE `stray` (
+  `id` int(255) NOT NULL,
+  `memberID` int(255) NOT NULL,
+  `title` varchar(20) DEFAULT NULL,
+  `type` enum('狗','貓') NOT NULL,
+  `ear` enum('是','否') NOT NULL,
+  `chip` enum('是','否') NOT NULL,
+  `ligation` enum('是','否') CHARACTER SET utf8mb4 NOT NULL,
+  `nickname` varchar(255) DEFAULT NULL,
+  `gender` enum('男','女') NOT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `town` varchar(255) DEFAULT NULL,
+  `address` varchar(40) DEFAULT NULL,
+  `appearance` varchar(100) DEFAULT NULL,
+  `img` varchar(100) DEFAULT NULL,
+  `contact` varchar(30) DEFAULT NULL,
+  `reason` varchar(100) DEFAULT NULL,
+  `detail` varchar(255) CHARACTER SET utf8mb4 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `stray`
+--
+
+INSERT INTO `stray` (`id`, `memberID`, `title`, `type`, `ear`, `chip`, `ligation`, `nickname`, `gender`, `city`, `town`, `address`, `appearance`, `img`, `contact`, `reason`, `detail`) VALUES
+(1, 0, '我家愛貓遺失了 請求幫助協尋!!!', '', '是', '是', '', 'Uni(屋逆)', '男', '5', '3', '227 Sec3 Taiwan Blvd', 'black and huge', 'images/stry1.png', 'cell phone 0123456789', 'lost at central park this afternoon 20210101', ''),
+(2, 0, '黑狗娜娜走失了', '狗', '是', '是', '', '娜娜', '女', '5', '3', '306 Sec1 Taiwan Blvd', 'black and huge', 'images/stry2.png', 'cell phone 0123456789', 'near central park this afternoon 20210102', ''),
+(3, 0, '家犬走失，感謝金一萬，拜託大家', '狗', '是', '是', '', '吉利', '男', '5', '3', '808 Sec6 Taiwan Blvd', 'black and tiny', 'images/stry3.png', 'cell phone 0123456789', 'near central park this afternoon 20210103', ''),
+(4, 0, 'TEST', '狗', '是', '是', '', 'amy', '男', '5', '3', '808 Sec6 Taiwan Blvd', 'TEST', 'TEST', 'cell phone 0123456789', 'near central park this afternoon 20210103', ''),
+(15, 0, 's', '狗', '是', '是', '是', 'qw', '男', '高雄市', '', 'wqe', 'wqe', './upload/20220305_210904.jpg', 'qwe', NULL, 'wqe'),
+(16, 0, 's', '狗', '是', '是', '是', 'qw', '男', '臺南市', '中西區', 'wqe', 'wqe', './upload/20220305_210947.jpg', 'qwe', NULL, 'wqe');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -151,7 +147,7 @@ CREATE TABLE `users` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- 傾印資料表的資料 `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`memberID`, `account`, `password`, `name`, `sex`, `birthday`, `cellphone`, `hide`, `city`, `town`, `img`) VALUES
@@ -162,57 +158,57 @@ INSERT INTO `users` (`memberID`, `account`, `password`, `name`, `sex`, `birthday
 (12, '1013@mail.com', '123', '梁建功2', '男', '2022-03-05', NULL, NULL, NULL, NULL, './images/indexlogo.jpg');
 
 --
--- 已傾印資料表的索引
+-- Indexes for dumped tables
 --
 
 --
--- 資料表索引 `adopt`
+-- Indexes for table `adopt`
 --
 ALTER TABLE `adopt`
   ADD PRIMARY KEY (`id`);
 
 --
--- 資料表索引 `cofind`
---
-ALTER TABLE `cofind`
-  ADD PRIMARY KEY (`id`);
-
---
--- 資料表索引 `rescue`
+-- Indexes for table `rescue`
 --
 ALTER TABLE `rescue`
   ADD PRIMARY KEY (`id`);
 
 --
--- 資料表索引 `users`
+-- Indexes for table `stray`
+--
+ALTER TABLE `stray`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`memberID`);
 
 --
--- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- 使用資料表自動遞增(AUTO_INCREMENT) `adopt`
+-- AUTO_INCREMENT for table `adopt`
 --
 ALTER TABLE `adopt`
-  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- 使用資料表自動遞增(AUTO_INCREMENT) `cofind`
---
-ALTER TABLE `cofind`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- 使用資料表自動遞增(AUTO_INCREMENT) `rescue`
+-- AUTO_INCREMENT for table `rescue`
 --
 ALTER TABLE `rescue`
-  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
--- 使用資料表自動遞增(AUTO_INCREMENT) `users`
+-- AUTO_INCREMENT for table `stray`
+--
+ALTER TABLE `stray`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `memberID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
