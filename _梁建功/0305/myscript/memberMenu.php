6e0a191d8668a202f8ce5aa96eaedbe8ff473de8
NOMO1013 <98079676+NOMO1013@users.noreply.github.com>
2022-03-06 00:18:26 +0800

    Add files via upload

---
+++ _梁建功/0305/myscript/memberMenu.php
@@ -0,0 +1,31 @@
+<div class="menu-box">
+    <div class="title-box">
+        <h5 class="title">
+            功能選單
+        </h5>
+    </div>
+    <ul class="menu-unstyled">
+        <li>
+            <a href="./member.php" title="會員個人頁面">會員個人頁面</a>
+        </li>
+        <li>
+            <a href="./edit.php" title="會員基本資料">會員基本資料</a>
+        </li>
+        <li>
+            <a href="./rescue_manager.php" title="會員基本資料">流浪救援管理</a>
+        </li>
+        <li>
+            <a href="./adopt_manager.php" title="會員基本資料">動物送養管理</a>
+        </li>
+        <li>
+            <a href="./mypet_manager.php" title="會員基本資料">動物領養管理</a>
+        </li>
+        <li>
+            <a href="./stray_manager.php" title="會員基本資料">遺失協尋管理</a>
+        </li>
+    </ul>
+    <div>
+        <a href="./logout.php" title="登出" class="logout-btn">登出<i
+                class="fa fa-sign-out pl-10"></i></a>
+    </div>
+</div>
---
+++ _梁建功/0305/stray.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<title>浪跡天涯 | 遺失協尋</title>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="assets/css/main.css" />
+	<script src="assets/js/jquery.min.js"></script>
+
+</head>
+
+<body>
+
+
+	<div id="page-wrapper">
+
+		<!-- 匯入NAVBAR  -->
+		<div id="navbar">
+		</div>
+
+		<!-- Main -->
+		<div id="main">
+			<div class="container">
+				<h2>遺失協尋</h2>
+				<div class="row main-row">
+
+					<!-- 匯入RESEARCH -->
+					<!-- 搜尋列 -->
+					<div id="research" class="col-8">
+					</div>
+
+					<!-- 最新公佈欄 -->
+					<div class=" col-md-9 col-sm-12  ">
+
+						<section>
+
+							<ul class="big-image-list">
+								<!-- 第一筆 -->
+								<?php
+								//// 資料庫連線使用外部匯入
+								require_once("dbtools.inc.php");
+								$link = create_connection();    /// 設定連線的資料庫(外部匯入)
+								$sql = "select * from cofind order BY id desc";  /// 要做的SQL內容 >> 從RESCUE中取得全部資料  
+								$result = execute_sql($link, "wandering", $sql);   ///  取得更新後的內容(外部匯入)
+								?>
+
+								
+								<?php
+								while ($row = mysqli_fetch_object($result)) {   ///  將更新後的內容每筆根據HTML條列
+								?>
+									<div class="card mb-3" style="max-width: 800px;">
+										<div class="row g-0">
+											<div class="col-md-4">
+												<a href='stray_showDetail.php?id=<?php echo	$row->id ?> '>
+													<img src="<?php echo    $row->img ?>" type="button" style="padding: 20px;height: auto; width: auto" class="img-fluid rounded-start" alt="...">
+												</a>
+											</div>
+											<div class="col-md-8">
+												<div class="card-body">
+													<a href='stray_showDetail.php?id= <?php echo 	$row->id ?> '>
+														<h5 class="card-title"> <?php echo   $row->title ?>
+														</h5>
+													</a>
+													<h4>
+														<div class="title">協尋案件編號：stray <?php echo    $row->id ?>
+														</div>
+													</h4>
+													<div class="datecreate mb-1">
+														<b class="card-text"> 動物類別：<?php echo    $row->type ?>
+															<br>
+															動物性別： <?php echo    $row->gender ?>
+														</b>
+														<br>
+														動物名稱：<?php echo    $row->name ?>
+														<br>
+														有無晶片：<?php echo    $row->chip ?>
+														<br><br>
+														<a href='stray_showDetail.php?id=<?php echo 	$row->id ?> '>詳細資料</a></td>
+														</tr>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								<?php }; ?>
+
+
+								<?php
+								mysqli_free_result($result);  /// 釋放暫存記憶體
+								mysqli_close($link);  /// 關閉連線
+								?>
+
+							</ul>
+						</section>
+					</div>
+					<!-- 最新公佈欄  END-->
+
+					<div class="col-md-3 col-sm-12 ">
+						<a href="strayForm.html">
+							<div class="btn btn-info mb-5 ">
+								<h2 class="text-white">新增遺失通報</h2>
+							</div>
+						</a>
+						<!-- 下半部右側相關連結 -->
+						<div id="rightspace"></div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- 匯入Footer  -->
+		<div id="footer">
+		</div>
+
+	</div>
+
+	<script>
+		$(document).ready(function() {
+			$("#navbar").load("myscript/navbar.php"); //以ID找DOM，更改裡面的html
+			$("#research").load("myscript/research.html"); //以ID找DOM，更改裡面的html
+			$("#rightspace").load("myscript/rightspace.html"); //以ID找DOM，更改裡面的html    右側連結
+			$("#footer").load("myscript/footer.html"); //以ID找DOM，更改裡面的html
+		});
+	</script>
+
+
+
+</body>
+
+</html>
