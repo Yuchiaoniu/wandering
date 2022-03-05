a678e33bb3b02caa0d73b5ff469d1a73bd82e607
Yuchiaoniu <97222721+Yuchiaoniu@users.noreply.github.com>
2022-03-05 22:08:33 +0800

    Add files via upload

---
+++ 大專整合_V3/myscript/banner.php
@@ -0,0 +1,40 @@
+<div id="banner-wrapper">
+	<div class="container">
+		<div id="banner" style="padding:2% 2%;">
+			<div id="carousel">
+				<div id="playList" class="carousel slide carousel-fade" data-ride="carousel" style="width: 100%;">
+					<!-- Indicators -->
+					<ul class="carousel-indicators">
+						<li data-target="#playList" data-slide-to="0" class="active" style="background-color:red;"></li>
+						<li data-target="#playList" data-slide-to="1" style="background-color:red;"></li>
+						<li data-target="#playList" data-slide-to="2" style="background-color:red;"></li>
+					</ul>
+					<!-- The slideshow -->
+					<div class="carousel-inner" >
+						<div class="carousel-item active c1">
+							<img src="./images/doga.jpg">
+						
+						<div class="carousel-item c2">
+							<img src="./images/dogb.jpg">
+						
+						<div class="carousel-item c3">
+							<img src="./images/catc.jpg">
+						
+					
+					<!-- Left and right controls -->
+					<a class="carousel-control-prev" href="#playList" data-slide="prev">
+						<span class="carousel-control-prev-icon"></span>
+					</a>
+					<a class="carousel-control-next" href="#playList" data-slide="next">
+						<span class="carousel-control-next-icon"></span>
+					</a>
+				
+			
+		
+	
+
+<!-- Scripts -->
+<script src="./assets/js/popper.min.js"></script>
+<script src="./assets/js/bootstrap.min.js"></script>
+<!-- <script src="/assets\js\popper.min.js"></script>
+<script src="/assets\js/bootstrap.min.js"></script> -->
