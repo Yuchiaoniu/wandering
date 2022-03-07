57a7834e0664d9b0fa77e6da6174e8fc7baa7067
Yuchiaoniu <97222721+Yuchiaoniu@users.noreply.github.com>
2022-03-07 09:33:15 +0800

    Add files via upload

---
+++ 期末專題牛/20220307/addrescueForm.php
@@ -0,0 +1,89 @@
+<?php
+  require_once("dbtools.inc.php");
+  
+  
+
+  //建立資料連接
+  $link = create_connection();
+
+  //取得表單資料
+  $title = $_POST["title"];
+  $type = $_POST["type"]; 
+  $amount = $_POST["amount"]; 
+  $city = $_POST["city"];
+  $town = $_POST["town"]; 
+  $address = $_POST["address"]; 
+  $lost = $_POST["lost"];
+  $reason = $_POST["reason"];
+	$detail = $_POST["detail"];
+
+  if(isset($_POST['responsibility'])){
+    $responsibility = $_POST['responsibility'];
+}else{
+    $responsibility = "無";#default value
+}
+
+if(isset($_POST['need'])){
+  $need = $_POST['need'];
+}else{
+  $need = "無";#default value
+}
+
+// if(isset($_POST['img'])){
+//   $img = $_POST['img'];
+// }else{
+//   $img = "x";#default value
+// }
+
+  // 將陣列變成個別字串以解決上傳時array covert to string的問題
+  // $r = $_REQUEST['responsibility'];
+  // $n = $_REQUEST['need'];
+  
+  $nd = implode(', ',$need);
+  $ry = implode(', ',$responsibility);
+
+
+  //圖片上傳
+  $upload = $_FILES['upload'];
+    if ($upload['error'] == 0){
+        // $i
+        move_uploaded_file($upload['tmp_name'],
+            './upload/' . createFilename($upload['name'], $index));
+        
+    }
+
+     ////  更改檔名函數
+     function createFilename($source, $index){
+      date_default_timezone_set('Asia/Taipei');
+      $data = explode('.',$source);
+      if (count($data)>=2){
+          $sname = '.' . $data[count($data)-1] ;
+      }else{
+          $sname = '';
+      }
+      $filename = date('Ymd_His') . $index  . $sname;
+      return $filename;
+  }
+
+    // 將資料夾路徑回寫給變數  新增到資料庫
+    $img =  './upload/' . createFilename($upload['name'], $index) ;
+
+    // $img = "upload/{$upload['name']}" ;
+
+    // echo '<br>' .$img ;
+  
+		
+    //執行 SQL 命令，新增此帳號
+    $sql = "INSERT INTO rescue (title, type, amount, city, town, address, lost, need, reason, detail, img, responsibility) 
+			VALUES ('$title', '$type', '$amount', '$city', '$town', '$address', 
+              '$lost', '$nd', '$reason', 
+			        '$detail','$img', '$ry');";
+
+    $result = execute_sql($link, "wandering", $sql);
+	
+  //關閉資料連接	
+  mysqli_close($link);
+
+  // 重新導向回rescue.php 檢視頁面
+  header("location:rescue.php");
+?>
